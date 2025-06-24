b61231c62d5f0efea07a86dc5925e7a482e8f1d6
Ilya Zhukov <i.zhukov@fz-juelich.de>
2025-06-24 16:29:52 +0200

    Fix typo

--- src/theme/DocPaginator/index.tsx
+++ src/theme/DocPaginator/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
-import {Comments} from '@site/src/components/Comments';
+import Comments from "@site/src/components/Comments";
 
 export default function DocPaginatorWrapper(props) {
     return (
