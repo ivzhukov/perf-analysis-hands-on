ce1c9bd1a313cb7c3afa57485e7239b7876ea4a8
Ilya Zhukov <i.zhukov@fz-juelich.de>
2025-06-24 14:37:43 +0200

    Update comment event

--- src/theme/DocItem/Footer/index.tsx
+++ src/theme/DocItem/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import DocItemFooter from "@theme-original/DocItem/Footer";
 import type DocItemFooterType from "@theme/DocItem/Footer";
 import type { WrapperProps } from "@docusaurus/types";
-import { useDoc } from "@docusaurus/plugin-content-docs/client";
+import { useActiveDocContext } from "@docusaurus/plugin-content-docs/client";
 import Comments from "@site/src/components/Comments";
 
 type Props = WrapperProps<typeof DocItemFooterType>;
